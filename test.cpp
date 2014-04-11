925a726c785005129de53f60325801b4ef29abd9
Francis Gravel St-Pierre <gspfranc@gmail.com>
2014-04-10 20:51:40 -0400

    indentation

--- test.cpp
+++ test.cpp
@@ -12,14 +12,14 @@
 #include <math.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <iostream>
 #include <signal.h>
 #include <iostream>
-#include <fstream>  
+#include <fstream>
 
 
 #define PORT_NUM 5000
@@ -41,20 +41,20 @@ void doprocessing (int sock)
         exit(1);
     }
     printf("[SERVER] Request Received: %s\n",buffer);
-	std::ifstream myfile (buffer);
-	std::string line;
-	
-  if (myfile.is_open())
-  {
-    while ( getline (myfile,line) )
+    std::ifstream myfile (buffer);
+    std::string line;
+
+    if (myfile.is_open())
     {
-      n = write(sock,line.c_str(),line.size());
+        while ( getline (myfile,line) )
+        {
+            n = write(sock,line.c_str(),line.size());
+        }
+        myfile.close();
     }
-    myfile.close();
-  }
-	
+
     n = write(sock,"[[file.txt]]",12);
-    if (n < 0) 
+    if (n < 0)
     {
         printf("printf writing to socket");
         exit(1);
@@ -62,37 +62,38 @@ void doprocessing (int sock)
 }
 
 int sockfd, newsockfd, no_de_port;
-void *Server(void *threadid){
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in adresse_serveur, adresse_client;
-     int n;
+void *Server(void *threadid)
+{
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in adresse_serveur, adresse_client;
+    int n;
 
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
         printf("Impossible d'ouvrir la socket \n");
-	
-	//Remplir de zero
-     memset((char *) &adresse_serveur,0, sizeof(adresse_serveur));
-	 
-     no_de_port = PORT_NUM;
-	 
-     adresse_serveur.sin_family = AF_INET;
-     adresse_serveur.sin_addr.s_addr = INADDR_ANY;
-     adresse_serveur.sin_port = htons(no_de_port);
-	
-	//Binder la socket
-     if (bind(sockfd, (struct sockaddr *) &adresse_serveur,
-              sizeof(adresse_serveur)) < 0) 
-              printf("Impossible de binder la socket \n");
-     listen(sockfd,5);
-	 
+
+    //Remplir de zero
+    memset((char *) &adresse_serveur,0, sizeof(adresse_serveur));
+
+    no_de_port = PORT_NUM;
+
+    adresse_serveur.sin_family = AF_INET;
+    adresse_serveur.sin_addr.s_addr = INADDR_ANY;
+    adresse_serveur.sin_port = htons(no_de_port);
+
+    //Binder la socket
+    if (bind(sockfd, (struct sockaddr *) &adresse_serveur,
+             sizeof(adresse_serveur)) < 0)
+        printf("Impossible de binder la socket \n");
+    listen(sockfd,5);
+
     clilen = sizeof(adresse_client);
     while (1) //Pour toujours
     {
-		//ACCEPT
-        newsockfd = accept(sockfd, 
-                (struct sockaddr *) &adresse_client, &clilen);
+        //ACCEPT
+        newsockfd = accept(sockfd,
+                           (struct sockaddr *) &adresse_client, &clilen);
         if (newsockfd < 0)
         {
             printf("ERROR on accept");
@@ -103,9 +104,9 @@ void *Server(void *threadid){
         if (pid < 0)
         {
             printf("ERROR on fork");
-	    exit(1);
+            exit(1);
         }
-        if (pid == 0)  
+        if (pid == 0)
         {
             /* C'est le client */
             close(sockfd);
@@ -114,108 +115,114 @@ void *Server(void *threadid){
         }
         else
         {
-			//fermeture des sockets
+            //fermeture des sockets
             close(newsockfd);
         }
     } /* end of while */
 }
 
- void cleanExit(){
-	printf("Kill signal received... closing sockets");
-	close(sockfd);
-	close(newsockfd);
-	exit(0);
- }
- 
- 
-  void *Client(void *threadid){
-     int sockfd, no_de_port, n;
+void cleanExit()
+{
+    printf("Kill signal received... closing sockets");
+    close(sockfd);
+    close(newsockfd);
+    exit(0);
+}
+
+
+void *Client(void *threadid)
+{
+    int sockfd, no_de_port, n;
     struct sockaddr_in adresse_serveur;
     struct hostent *server;
-	std::ofstream myfile;
+    std::ofstream myfile;
 
     char buffer[256];
-   
+
     no_de_port = PORT_NUM;
-	/** INIT **/
+    /** INIT **/
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         printf("Impossible de créer la socket");
-   
-   server = gethostbyname("localhost");
-    if (server == NULL) {
+
+    server = gethostbyname("localhost");
+    if (server == NULL)
+    {
         fprintf(stderr,"impossible de contacter l'host \n");
         exit(0);
     }
     memset((char *) &adresse_serveur,0, sizeof(adresse_serveur));
     adresse_serveur.sin_family = AF_INET;
     memcpy((char *)&adresse_serveur.sin_addr.s_addr,
-	       (char *)server->h_addr,
-         server->h_length);
+           (char *)server->h_addr,
+           server->h_length);
     adresse_serveur.sin_port = htons(no_de_port);
-    
-	/** CONNECT **/
-	if (connect(sockfd,(struct sockaddr *) &adresse_serveur,sizeof(adresse_serveur)) < 0) 
+
+    /** CONNECT **/
+    if (connect(sockfd,(struct sockaddr *) &adresse_serveur,sizeof(adresse_serveur)) < 0)
         printf("printf connecting");
     memset(buffer,0,256);
-	
-	
-	/** WRITE **/
+
+
+    /** WRITE **/
     n = write(sockfd,"file.txt",8);
-    if (n < 0) 
-         printf("Impossible d'envoyer la request");
+    if (n < 0)
+        printf("Impossible d'envoyer la request");
 
     n = read(sockfd,buffer,255);
-    if (n < 0) 
-         printf("Impossible de lire la reponse de la socket");
+    if (n < 0)
+        printf("Impossible de lire la reponse de la socket");
     printf("[CLIENT] I received: %s\n",buffer);
 
-	myfile.open("received.txt");
-	//Écriture dans le fichier
-	myfile << buffer;
-	myfile.close();
+    myfile.open("received.txt");
+    //Écriture dans le fichier
+    myfile << buffer;
+    myfile.close();
     close(sockfd);
     return 0;
-  pthread_exit(NULL);
+    pthread_exit(NULL);
 
-  }
+}
 
 
 
 
 main()
-{  
+{
 //***** Création des thread ****//
-pthread_t serveur;
-pthread_t clients[NUM_CLIENTS];
+    pthread_t serveur;
+    pthread_t clients[NUM_CLIENTS];
 
 
 //***Enregistrement des handler de signals***//
 // signal(SIGINT,cleanExit);
- 
 
 
-int rc;
-int t;
 
-	rc = pthread_create(&serveur, NULL, Server, (void *)t);
-      if (rc){
-         printf(" Impossible de démarer le serveur: %d\n", rc);
-         exit(-1);
-      }
-	  printf("Server is started \n");
+    int rc;
+    int t;
+
+    rc = pthread_create(&serveur, NULL, Server, (void *)t);
+    if (rc)
+    {
+        printf(" Impossible de démarer le serveur: %d\n", rc);
+        exit(-1);
+    }
+    printf("Server is started \n");
 
 
-   for(t=0; t<NUM_CLIENTS; t++){
-      rc = pthread_create(&clients[t], NULL, Client, (void *)t);
-      if (rc){
-         printf(" Impossible de créer le client:  %d\n", rc);
-         exit(-1);
-      }
-	  printf("Client is started: %ld\n", t+1);
+    for(t=0; t<NUM_CLIENTS; t++)
+    {
+        rc = pthread_create(&clients[t], NULL, Client, (void *)t);
+        if (rc)
+        {
+            printf(" Impossible de créer le client:  %d\n", rc);
+            exit(-1);
+        }
+        printf("Client is started: %ld\n", t+1);
+    }
+    pthread_exit(NULL);
+
+
+
 }
-  pthread_exit(NULL);
- 
- 
- 
- }
